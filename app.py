8bdda5edc472bc40084dd8c77ba2c33e583fbd77
Mikefromlanco <mikebainer@gmail.com>
2025-08-02 13:10:44 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
+import streamlit as st
+from datetime import date
+
+# Set initial section if not already set
 if "active_section" not in st.session_state:
     st.session_state.active_section = "Child Demographics"
 
+# ---------- Page Setup ----------
+st.set_page_config(page_title="MOSES™ – Gross Motor Inventory", layout="wide")
